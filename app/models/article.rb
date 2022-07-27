6ed0f0ec957e1add8ea0639574925897663b1147
gabdykalyk <gabdykalyk@gmail.com>
2022-07-27 12:15:45 +0600

    error page finished

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ApplicationRecord
   #image validations
   validates_presence_of :image
   validates_size_of :image, maximum: 500.kilobytes,
-                    message: "should not be more than 400KB", if: :image_changed?
+                    message: "should not be more than 500KB", if: :image_changed?
  
   validates_property :format, of: :image, in: ['jpeg', 'png'],
                       message: "the formats allowed are: .jpeg, .png", if: :image_changed?
